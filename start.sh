7732f0bc428907ab0a644105929acaca213b41c4
Randy Zhang <ranzhang@users.noreply.github.com>
2018-07-27 15:28:21 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+##MacBook: en0 and en1 may be wired/wireless interfaces
 ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 sed -i "s/DOCKER_HOST_IP/$ip/g" prometheus.yml
 docker-compose up -d --build
